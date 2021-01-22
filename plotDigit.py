1b8f6fe0be61f4de1a10d47f46959e976a608fa6
TRebekka <49750757+TRebekka@users.noreply.github.com>
2021-01-22 11:39:55 +0100

    Update plotDigit.py

--- plotDigit.py
+++ plotDigit.py
@@ -1,15 +1,34 @@
+# This file is used to take a sample from the dataset to be shown in the report.
+
+# import libraries
 import numpy as np
 from matplotlib import pyplot as plt
+import random
 
 # load data
 data = np.genfromtxt("mfeat-pix.txt", dtype=None)
 
-# make a 2d array from a 1d array
-first_digit = np.reshape(data[0], (16, 15))
+# set parameters
+n_cols = 3
+n_rows = 5
+indices = random.sample(range(200), n_rows)
 
-# print values of first digit
-print(first_digit)
+# initialize subplots
+fig, axes = plt.subplots(n_cols, n_rows, sharex=True, sharey=True, gridspec_kw={'hspace': 0, 'wspace': 0})
 
-# show first digit using the reversed gray map
-plt.imshow(first_digit, cmap='gray_r', vmin=0, vmax=6)
+# fill the figure
+for x in range(n_cols):
+    for y in range(n_rows):
+        # make a 2d array from a 1d array
+        index = indices[y] + x * 800 + 200
+        first_digit = np.reshape(data[index], (16, 15))
+
+        # show image
+        axes[x,y].imshow(first_digit, cmap='gray_r')
+
+        # remove ticks
+        axes[x,y].set_xticks([])
+        axes[x,y].set_yticks([])
+
+# show figure
 plt.show()
